dbad162dcd27619127aa23cd7400510179b4caf7
Lucas <lucasumberger@gmail.com>
2023-08-03 17:31:59 -0400

    Create functions getRndInteger, getComputerChoice

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Script javascript file for the Rock Paper Scissors Game */
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+function getComputerChoice() {
+    let randomNumber = getRndInteger(1,3);
+    if (randomNumber === 1) {
+        return 'Rock';
+    }
+    else if (randomNumber === 2) {
+        return 'Paper';
+    }
+    else
+        return 'Scissors';
+}
