61fd4f894f68a5016d903b06d5b30ef0ae5d3c4d
Lucas <lucasumberger@gmail.com>
2023-08-03 18:01:12 -0400

    Create playRound function

--- script.js
+++ script.js
@@ -7,11 +7,44 @@ function getRndInteger(min, max) {
 function getComputerChoice() {
     let randomNumber = getRndInteger(1,4);
     if (randomNumber === 1) {
-        return 'Rock';
+        return 'rock';
     }
     else if (randomNumber === 2) {
-        return 'Paper';
+        return 'paper';
     }
     else
-        return 'Scissors';
+        return 'scissors';
+}
+
+function playRound(computerSelection, playerSelection) {
+    let playerSelection = playerSelection.toLowerCase();
+    if (computerSelection === 'rock') {
+        if (playerSelection === 'rock') {
+            return 'Thats a Tie! Both you and the computer chose Rock!';
+        }
+        else if (playerSelection === 'scissors') {
+            return 'You Lose! Rock beats Scissors!';
+        }
+        else
+            return 'You Win! Paper beats Rock';
+    }
+    else if (computerSelection === 'scissors') {
+        if (playerSelection === 'rock') {
+            return 'You Win! Rock beats Scissors!';
+        }
+        else if (playerSelection === 'scissors') {
+            return 'Thats a Tie! Both you and the computer chose Scissors!';
+        }
+        else
+            return 'You Lose! Scissors beats Paper!';
+    }
+    else
+        if (playerSelection === 'rock') {
+            return 'You Win! Rock beats Paper!';
+        }
+        else if (playerSelection === 'scissors') {
+            return 'You Lose! Paper beats Rock!';
+        }
+        else
+            return 'Thats a Tie! Both you and the computer chose Paper!';
 }
