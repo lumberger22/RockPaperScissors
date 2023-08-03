561c7f5357be77da6c5b82c6c6b5d96c65c860e1
Lucas <lucasumberger@gmail.com>
2023-08-03 19:02:15 -0400

    Create game function

--- script.js
+++ script.js
@@ -16,8 +16,10 @@ function getComputerChoice() {
         return 'scissors';
 }
 
-function playRound(computerSelection, playerSelection) {
-    let playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = getComputerChoice();
+
     if (computerSelection === 'rock') {
         if (playerSelection === 'rock') {
             return 'Thats a Tie! Both you and the computer chose Rock!';
@@ -31,6 +33,7 @@ function playRound(computerSelection, playerSelection) {
     else if (computerSelection === 'scissors') {
         if (playerSelection === 'rock') {
             return 'You Win! Rock beats Scissors!';
+            let result = win;
         }
         else if (playerSelection === 'scissors') {
             return 'Thats a Tie! Both you and the computer chose Scissors!';
@@ -38,13 +41,38 @@ function playRound(computerSelection, playerSelection) {
         else
             return 'You Lose! Scissors beats Paper!';
     }
-    else
+    else {
         if (playerSelection === 'rock') {
-            return 'You Win! Rock beats Paper!';
+            return 'You Lose! Rock beats Paper!';
         }
         else if (playerSelection === 'scissors') {
-            return 'You Lose! Paper beats Rock!';
+            return 'You Win! Scissors beats paper!';
         }
         else
             return 'Thats a Tie! Both you and the computer chose Paper!';
+    }
+}
+
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        let result = playRound(playerSelection);
+        console.log(result);
+        if (result.substring(0,5) === 'Thats') {
+            console.log("Computer: " + computerScore + " | player: " + playerScore);
+        }
+        else if (result.substring(0,5) === 'You W') {
+            playerScore++;
+            console.log("Computer: " + computerScore + " | player: " + playerScore);
+        }
+        else {
+            computerScore++;
+            console.log("Computer: " + computerScore + " | player: " + playerScore);
+        }
+    }
+
+    console.log("The final score was, computer: " + computerScore + " | player: " + playerScore);
 }
